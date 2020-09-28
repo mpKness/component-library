4f3125c612cead63556c31c4bcb88ea9d7ffab8e
mpkness <mpkness@gmail.com>
2020-09-28 08:31:18 -0700

    initial commit

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  expect('This is a place for components').toBeInTheDocument();
+});
